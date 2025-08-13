cff78a24adced8882ec4cb710d26dd2708eb9418
Jens <jens.nomtak@readly.com>
2025-08-13 22:41:51 +0200

    change hostname

--- machines/nix-vm-hyperv/default.nix
+++ machines/nix-vm-hyperv/default.nix
@@ -13,7 +13,7 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-  networking.hostName = "nixos-vm-hyperv"; # Define your hostname.
+  networking.hostName = "nix-vm-hyperv"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
